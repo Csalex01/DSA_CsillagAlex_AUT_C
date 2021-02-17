ae6536001d4941281f03990797952d269c62708c
Csillag Alex <csillagalex2001@gmail.com>
2021-02-17 10:33:13 +0200

    3. feladat teljesítve.

--- 3_feladat/main.c
+++ 3_feladat/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int ossz(int a, int b);
+
+int main()
+{
+    int a, b;
+
+    printf("a: ");
+    scanf("%i", &a);
+
+    printf("b: ");
+    scanf("%i", &b);
+
+    printf("%i + %i = %i", a, b, ossz(a, b));
+}
+
+int ossz(int a, int b)
+{
+    return a + b;
 }
