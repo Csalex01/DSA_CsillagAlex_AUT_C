ba23ec4893808d8b286f8e6da268634c47fcaed0
Csillag Alex <csillagalex2001@gmail.com>
2021-03-24 19:09:53 +0200

    Dynamic Queue

---
+++ Labor_6/Queue_Dynamic/cmake-build-debug/CMakeFiles/Queue_Dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Queue_Dynamic.dir/main.c.obj"
+  "Queue_Dynamic.exe"
+  "Queue_Dynamic.exe.manifest"
+  "Queue_Dynamic.pdb"
+  "libQueue_Dynamic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Queue_Dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Labor_6/Queue_Dynamic/cmake-build-debug/CMakeFiles/Queue_Dynamic.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Queue_Dynamic.
+# This may be replaced when dependencies are built.
