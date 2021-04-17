b3f550b196d6021027c36f566dfe6c60753f3843
Csillag Alex <csillagalex2001@gmail.com>
2021-04-18 01:25:43 +0300

    Sorted Linked List

---
+++ Labor_8/Sorted_Linked_List/cmake-build-debug/CMakeFiles/Sorted_Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorted_Linked_List.dir/main.c.obj"
+  "CMakeFiles/Sorted_Linked_List.dir/sorted_linked_list.c.obj"
+  "Sorted_Linked_List.exe"
+  "Sorted_Linked_List.exe.manifest"
+  "Sorted_Linked_List.pdb"
+  "libSorted_Linked_List.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sorted_Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
