31243e7e8876a7c4daa4c6f12c350f978b77aee6
Csillag Alex <csillagalex2001@gmail.com>
2021-04-20 17:32:09 +0300

    A csoport feladatainak megoldása

---
+++ 2021_I_Parcialis_A/cmake-build-debug/CMakeFiles/2021_I_Parcialis_A.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "2021_I_Parcialis_A.exe"
+  "2021_I_Parcialis_A.exe.manifest"
+  "2021_I_Parcialis_A.pdb"
+  "CMakeFiles/2021_I_Parcialis_A.dir/linked_list.c.obj"
+  "CMakeFiles/2021_I_Parcialis_A.dir/main.c.obj"
+  "CMakeFiles/2021_I_Parcialis_A.dir/stack.c.obj"
+  "lib2021_I_Parcialis_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2021_I_Parcialis_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
