8687ef3910850caf5d2c77612d84103efee757b2
Csalex01 <csillagalex2001@gmail.com>
2021-05-19 11:47:41 +0300

    Solution for the first problem

---
+++ Labor_13/HashTable/cmake-build-debug/CMakeFiles/HashTable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashTable.dir/functions.c.obj"
+  "CMakeFiles/HashTable.dir/main.c.obj"
+  "HashTable.exe"
+  "HashTable.exe.manifest"
+  "HashTable.pdb"
+  "libHashTable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HashTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
